2c781428bb8f79e6227c25560ecc0cdb463394d4
Manh Nguyen <manhbapper190704@gmail.com>
2025-04-29 22:19:51 +0700

    video 24

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+const express = require('express');
+
+const app = express();
+const port = 8080;
+app.get("/", (req, res) => {
+    res.send ("Hello World!");
+})
+app.get("/hoidanit", (req, res) => {
+    res.send ("I have a problem with my code!");
+})
+app.listen(port, () => {
+    console.log(`My app is running on port : ${port}`);
+})
