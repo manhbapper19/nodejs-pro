5ea4d323af5d9c75012ffcd982b8bf3808d01b64
Manh Nguyen <manhbapper190704@gmail.com>
2025-04-30 16:34:24 +0700

    video 29

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 app.get("/", (req, res) => {
-    res.send ("Hello World!");
+    res.send ("Hello World nodemon!");
 })
 app.get("/hoidanit", (req, res) => {
     res.send ("I have a problem with my code!");
